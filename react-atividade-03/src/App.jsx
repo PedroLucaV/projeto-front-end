60f0cf960181db6bbe4fad5e52052c43ffc7f651
Pedro <izielucas2016@gmail.com>
2024-04-01 12:44:34 -0300

    atv 03 updated

--- react-atividade-03/src/App.jsx
+++ react-atividade-03/src/App.jsx
@@ -7,12 +7,12 @@ const App = () => {
                         <p>Pedro Lucas</p>
                         <p className='location'>Maceio, Al</p>
                 </div>
-                <form action="">
-                    <input type="button" value="Github"/>
-                    <input type="button" value="Frontend Mentor"/>
-                    <input type="button" value="Linkedin"/>
-                    <input type="button" value="Twitter"/>
-                    <input type="button" value="Instagram"/>
+                <form action="" className='form'>
+                        <input type="button" value="Github"/>
+                        <input type="button" value="Frontend Mentor"/>
+                        <input type="button" value="Linkedin"/>
+                        <input type="button" value="Twitter"/>
+                        <input type="button" value="Instagram"/>
                 </form>
             </div>
         </>
