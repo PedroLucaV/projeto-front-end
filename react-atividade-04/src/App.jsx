13d3820d2d097da53ba8072b44f6c4ed82cefb3a
Pedro <izielucas2016@gmail.com>
2024-04-03 12:40:28 -0300

    Started

---
+++ react-atividade-04/src/App.jsx
@@ -0,0 +1,24 @@
+import data from './assets/data.json'
+import './style.css'
+import memory from './assets/assets/images/icon-memory.svg'
+import reaction from './assets/assets/images/icon-reaction.svg'
+import verbal from './assets/assets/images/icon-verbal.svg'
+import visual from './assets/assets/images/icon-visual.svg'
+
+const SummaryItems = (props) => {
+    return(
+        <>
+            <img src={props} alt="" />
+        </>
+    )
+}
+
+const App = () => {
+    return(
+        <main>
+
+        </main>
+    )
+}
+
+export default App;
